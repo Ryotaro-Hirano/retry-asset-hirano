7cf49adfca0e153440fb4de76faaa61a808ab7de
Ryotaro Hirano <ryotar0513jedi@gmail.com>
2018-04-15 23:59:56 +0900

    公開用

---
+++ js/rollover.js
@@ -0,0 +1,66 @@
+// 北京
+  var preImg=new Image();
+  preImg.src="img/portfolio-hr/ch-bj_on.jpg";
+
+  document.getElementById("chImg").onmouseover=function() {
+    document.getElementById("chImg").setAttribute("src","img/portfolio-hr/ch-bj_on.jpg");
+  }
+
+  document.getElementById("chImg").onmouseOut=function() {
+        document.getElementById("chImg").setAttribute("src","img/portfolio-hr/ch-bj.jpg");
+  }
+// ヴァチカン
+  var preImg=new Image();
+  preImg.src="img/portfolio-hr/vc-vachican_on.jpg";
+
+  document.getElementById("vcImg").onmouseover=function() {
+    document.getElementById("vcImg").setAttribute("src","img/portfolio-hr/vc-vachican_on.jpg");
+  }
+
+  document.getElementById("vcImg").onmouseOut=function() {
+        document.getElementById("vcImg").setAttribute("src","img/portfolio-hr/vc-vachican.jpg");
+  }
+// ローマ
+  var preImg=new Image();
+  preImg.src="img/portfolio-hr/it-collseoj_on.jpg";
+
+  document.getElementById("itImg").onmouseover=function() {
+    document.getElementById("itImg").setAttribute("src","img/portfolio-hr/it-collseo_on.jpg");
+  }
+
+  document.getElementById("itImg").onmouseOut=function() {
+        document.getElementById("itImg").setAttribute("src","img/portfolio-hr/it-collseo.jpg");
+  }
+// マック
+  var preImg=new Image();
+  preImg.src="img/portfolio-hr/ny-apple_on.jpg";
+
+  document.getElementById("nyImg").onmouseover=function() {
+    document.getElementById("nyImg").setAttribute("src","img/portfolio-hr/ny-apple_on.jpg");
+  }
+
+  document.getElementById("nyImg").onmouseOut=function() {
+        document.getElementById("nyImg").setAttribute("src","img/portfolio-hr/ny-apple.jpg");
+  }
+// センタラル
+  var preImg=new Image();
+  preImg.src="img/portfolio-hr/ny-central_on.jpg";
+
+  document.getElementById("nycImg").onmouseover=function() {
+    document.getElementById("nycImg").setAttribute("src","img/portfolio-hr/ny-central_on.jpg");
+  }
+
+  document.getElementById("nycImg").onmouseOut=function() {
+        document.getElementById("nycImg").setAttribute("src","img/portfolio-hr/ny-central.jpg");
+  }
+// 図書館
+  var preImg=new Image();
+  preImg.src="img/portfolio-hr/ny-libray_on.jpg";
+
+  document.getElementById("nylImg").onmouseover=function() {
+    document.getElementById("nylImg").setAttribute("src","img/portfolio-hr/ny-libray_on.jpg");
+  }
+
+  document.getElementById("nylImg").onmouseOut=function() {
+        document.getElementById("nylImg").setAttribute("src","img/portfolio-hr/ny-libray.jpg");
+  }
